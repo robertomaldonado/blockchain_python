6b4cfff043805eddbf6f8cb37968c8f47dba989d
Roberto <rbt.maldonado@gmail.com>
2023-08-15 11:09:40 -0500

    Moved files, reorganize by function

--- blockchain.py
+++ blockchain.py
@@ -3,10 +3,10 @@ import hashlib as hl
 from typing import List
 import json
 # import pickle
-from hash_util import hash_block
+from utility.hash_util import hash_block
 from block import Block
 from transaction import Transaction
-from verification import Verification
+from utility.verification import Verification
 
 MINING_REWARD = 10
 
