dd904aded3971f40e9d6a33ef5406e93d05ab93a
Roberto <rbt.maldonado@gmail.com>
2023-08-16 15:57:45 -0500

    Added wallet and mining to API

--- blockchain.py
+++ blockchain.py
@@ -125,7 +125,7 @@ class Blockchain:
 
     def mine_block(self) -> bool:
         '''Will be adding a new block'''
-        if self.hosting_node == None: return False
+        if self.hosting_node == None: return None
         last_block = self.__chain[-1]
         hashed_block = hash_block(last_block)
         proof = self.proof_of_work()
@@ -133,10 +133,10 @@ class Blockchain:
         copied_transactions = self.__open_transactions[:]
         for tx in copied_transactions:
             if not Wallet.verify_transaction(tx):
-                return False
+                return None
         copied_transactions.append(reward_transaction)
         block = Block(len(self.__chain), hashed_block, copied_transactions, proof)
         self.__chain.append(block)
         self.__open_transactions = []
         self.save_data()
-        return True
+        return block
